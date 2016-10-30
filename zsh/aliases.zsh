6778491d5aba1a371248c65ef344ce75243f3de0
Fuad Saud <fuadfsaud@gmail.com>
2016-10-30 17:49:31 -0200

    Extract zsh aliases and add zsh-fzf config

---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+alias bx="bundle exec"
+alias cdgr="cd \`git root\`"
+alias git="hub"
+alias mk="make"
+alias rk="rake"
+alias tmc="teamocil --here"
+alias vhalt="vagrant halt"
+alias vssh="vagrant ssh"
+alias vup="vagrant up"
+alias vi="vim"
+alias vundle="vim +PluginInstall +qall"
+alias yt="youtube-dl -t"
+
+alias cp="${aliases[cp]:-cp} -v"
+alias ln="${aliases[ln]:-ln} -v"
+alias mv="${aliases[mv]:-mv} -v"
+alias rm="${aliases[rm]:-rm} -v"
+
--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -14,30 +14,13 @@ fi
 
 stty -ixon # disable C-s
 
-alias bx="bundle exec"
-alias cdgr="cd \`git root\`"
-alias git="hub"
-alias mk="make"
-alias rk="rake"
-alias tmc="teamocil --here"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vup="vagrant up"
-alias vi="vim"
-alias vundle="vim +PluginInstall +qall"
-alias yt="youtube-dl -t"
-
-alias cp="${aliases[cp]:-cp} -v"
-alias ln="${aliases[ln]:-ln} -v"
-alias mv="${aliases[mv]:-mv} -v"
-alias rm="${aliases[rm]:-rm} -v"
-
 unsetopt correct
+
+source "${ZDOTDIR:-$HOME}/.zsh/aliases.zsh"
+source "${ZDOTDIR:-$HOME}/.zsh/fzf.zsh"
 
 # source completions
 compctl -g '~/.teamocil/*(:t:r)' teamocil
 compctl -g '~/.rubies/*(:t)' chruby
 
 chruby 2.3
-
-source ~/.zsh/fzf.zsh
