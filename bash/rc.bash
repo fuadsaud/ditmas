a01f04715eb3d45bdc23ed61e0bb93f4bf9e8479
Fuad Saud <fuadfsaud@gmail.com>
2016-12-19 21:50:09 -0200

    Load fasd in bash

--- bash/rc.bash
+++ bash/rc.bash
@@ -6,6 +6,16 @@ if [[ -s "${HOME}/.nurc" ]]; then
   source ~/.nurc
 fi
 
+fasd_cd() {
+  if [ $# -le 1 ]; then
+    fasd "$@"
+  else
+    local _fasd_ret="$(fasd -e echo "$@")"
+    [ -z "$_fasd_ret" ] && return
+    [ -d "$_fasd_ret" ] && cd "$_fasd_ret" || echo "$_fasd_ret"
+  fi
+}
+
 source ~/.zsh/aliases.zsh
 
 source /usr/local/opt/chruby/share/chruby/chruby.sh
