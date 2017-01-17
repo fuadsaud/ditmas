1b4f338df979ac9dce552fffb03c121f726490cf
Fuad Saud <fuadfsaud@gmail.com>
2017-01-17 14:23:33 -0200

    S    e    n    d    N    u    d    e    s

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -25,3 +25,5 @@ alias sf='fasd -sif'     # interactive file selection
 alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
 alias j='fasd_cd -d'
+
+alias nude="nu utils docker-env"
