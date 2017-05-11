75cdcc289ed1f1e413e72c77d1bff1985eb086fd
Fuad Saud <fuadfsaud@gmail.com>
2017-05-11 10:58:17 -0300

    Add eastwood to lein profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -5,7 +5,8 @@
                               :username [:gpg :env/artifacts_aws_access_key_id]
                               :passphrase [:gpg :env/artifacts_aws_secret_access_key]}]]
 
-  :plugins [[s3-wagon-private "1.2.0"]]}
+  :plugins [[s3-wagon-private "1.2.0"]
+            [jonase/eastwood "0.2.3"]]}
   :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
                    [refactor-nrepl "2.0.0-SNAPSHOT"]]
          :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
