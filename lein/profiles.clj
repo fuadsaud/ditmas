2c39769055c2321955f018b6e1a1906b7e06d83e
Fuad Saud <fuadfsaud@gmail.com>
2017-07-14 13:37:32 -0300

    Bump cljdev

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,13 +1,14 @@
 {:user
- {:dependencies [[cljdev "0.5.0-SNAPSHOT"]]
+ {:dependencies [[cljdev "0.6.3-SNAPSHOT"]]
   :injections   [(require 'nu)]
   :repositories [["nu-maven" {:url "s3p://nu-maven/releases/"
                               :username [:gpg :env/artifacts_aws_access_key_id]
                               :passphrase [:gpg :env/artifacts_aws_secret_access_key]}]]
 
   :plugins [[s3-wagon-private "1.2.0"]
-            [jonase/eastwood "0.2.3"]]}
-  :repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
-                   [refactor-nrepl "2.0.0-SNAPSHOT"]]
+            [jonase/eastwood "0.2.4"]
+            [lein-gorilla "0.4.0"]]}
+  :repl {:plugins [[cider/cider-nrepl "0.14.0-SNAPSHOT"]
+                   [refactor-nrepl "2.3.0-SNAPSHOT"]]
          :dependencies [[org.clojure/tools.nrepl "0.2.12"]]
          :repl-options {:timeout 120000}}}
