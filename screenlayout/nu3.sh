0aafa6c4af939ea6907fb0cfe64209e0c644ea7a
Fuad Saud <fuadfsaud@gmail.com>
2018-08-04 17:48:57 -0300

    Add screenlayout

---
+++ screenlayout/nu3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output HDMI-2 --off --output HDMI-1 --mode 1920x1080 --pos 2560x0 --rotate left --output DP-1 --off --output eDP-1 --primary --mode 1920x1080 --pos 640x1440 --rotate normal --output DP-2 --mode 2560x1440 --pos 0x0 --rotate normal
