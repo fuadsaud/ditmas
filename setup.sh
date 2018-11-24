8966e340ce5eea25f1812b09b57cbe1f6c2f6b24
Fuad Saud <fuadfsaud@gmail.com>
2018-11-23 22:14:44 -0200

    11/2018 setup fixes

--- setup.sh
+++ setup.sh
@@ -2,118 +2,158 @@
 
 set -euo pipefail
 
+function echoerr {
+  (>&2 echo $1)
+}
+
+function log {
+  echoerr "=== (houdini) ===> ${1}"
+}
+
+function lns-once {
+  if [[ ! -e $2 ]]; then
+    ln -s $1 $2
+  fi
+}
+
 CODE_DIR=~/Code
 REPO='https://github.com/fuadsaud/houdini'
 PROJECTS_DIR="$CODE_DIR/fuadsaud/"
 HOUDINI_DIR=~/.houdini
+CONFIG_DIR=~/.config
 VIM_DIR=~/.vim
 GIT_DIR=~/.git
 
 HOMEBREW_ZSH_BIN=/usr/local/bin/zsh
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if [[ ! -x "$(command -v brew)" ]]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew update
 
-brew install zsh \
-  bash \
-  ag \
-  asciinema \
-  awscli \
-  chruby \
-  cowsay \
-  ctags \
-  curl \
-  fzf \
-  git \
-  htop \
-  hub \
-  jq \
-  leiningen \
-  mas \
-  python \
-  python2 \
-  fasd \
-  p7zip \
-  reattach-to-user-namespace \
-  trash \
-  tree \
-  unrar \
-  tmate \
-  tmux \
-  youtube-dl
-
-brew tap neovim/neovim
-
-brew install --HEAD \
-  vim \
-  neovim \
-  ruby-install
-
-brew cask install \
-  java \
-  spotify \
-  virtualbox \
-  google-chrome \
-  google-drive \
-  vlc \
-  atom \
-  dropbox \
-  vagrant \
-  go \
-  heroku-toolbelt \
-  docker \
-  whatsapp \
-  transmission \
-  1password \
-  spectacle \
-  flux \
-  iterm2-beta \
-  karabiner-elements \
+brew_cask_formulae=(
+  1password
+  atom
+  docker
+  dropbox
+  figma
+  flux
+  google-chrome
+  google-backup-and-sync
+  iterm2
   itsycal
+  java
+  karabiner-elements
+  spectacle
+  spotify
+  transmission
+  vlc
+  whatsapp
+)
 
-local mac_store_apps=(
+brew cask install ${brew_cask_formulae[@]}
+
+brew_formulae=(
+  ag
+  asciinema
+  awscli
+  bash
+  chruby
+  clojure
+  cowsay
+  ctags
+  curl
+  fzf
+  git
+  heroku/brew/heroku
+  htop
+  hub
+  iproute2mac
+  jq
+  leiningen
+  mas
+  neovim/neovim/neovim
+  python
+  python2
+  fasd
+  reattach-to-user-namespace
+  trash
+  tree
+  unrar
+  tmux
+  vim
+  youtube-dl
+  homebrew/cask-drivers/yubico-authenticator
+  zsh
+)
+
+brew install ${brew_formulae[@]}
+brew install --HEAD ruby-install
+
+# fonts
+
+brew_fonts=(
+  font-fira-sans
+  font-fira-code
+  font-comic-neue
+)
+
+brew tap caskroom/fonts
+brew cask install ${brew_fonts[@]}
+
+# MAS apps
+
+log 'Installing MAS apps'
+
+mac_store_apps=(
   'Telegram Desktop'
   'Slack'
-  'Twitter'
   'Amphetamine'
   'Daisy Disk'
   'Reeder 3'
   'Pixelmator'
   'Irvue'
-  'Chegou'
-  'CloudApp'
   'Shazam'
-  'Wunderlist'
 )
 
-for appname in mac_store_apps; do
-  mas search $appname | head -n 1 | cut -f 1 -d ' ' | xargs mas install
+for appname in "${mac_store_apps[@]}"; do
+  log "Installing ${appname}"
+  mas search "${appname}" | head -n 1 | cut -f 4 -d ' ' | xargs mas install
 done
 
-# fonts
+# python
 
-brew tap caskroom/fonts
+python_packages=(
+  virtualenv
+  virtualenvwrapper
+  powerline-status
+  psutil
+  speedtest-cli
+  doge
+  neovim
+)
 
-brew cask install \
-  font-fira-sans \
-  font-fira-code \
-  font-comic-neue
+pip install --upgrade pip setuptools
+pip install ${python_packages[@]}
+pip3 install --upgrade pip setuptools
+pip3 install ${python_packages[@]}
 
-brew tap brona/iproute2mac
-brew install iproute2mac
+# ruby
+
+ruby-install -s /tmp/ruby-src ruby
+
+# links
 
 mkdir -p $PROJECTS_DIR
 
-git clone --recursive $REPO "$PROJECTS_DIR"
-
-ln -s $PROJECTS_DIR/houdini $HOUDINI_DIR
+lns-once $PROJECTS_DIR/houdini $HOUDINI_DIR
 
 # vim
 
-ln -s $HOUDINI_DIR/vim $VIM_DIR
-ln -s $VIM_DIR/rc.vim ~/.vimrc
-ln -s $VIM_DIR/grc.vim ~/.gvimrc
+lns-once $HOUDINI_DIR/vim $VIM_DIR
+lns-once $VIM_DIR/rc.vim ~/.vimrc
+lns-once $VIM_DIR/grc.vim ~/.gvimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -122,7 +162,9 @@ vim +PlugInstall +qall
 
 # neovim
 
-ln -s $HOUDINI_DIR/nvim ~/.config/
+mkdir -p ${CONFIG_DIR}
+
+lns-once $HOUDINI_DIR/nvim ${CONFIG_DIR}
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -131,89 +173,61 @@ nvim +PlugInstall +qall
 
 # zsh
 
-ln -s $HOUDINI_DIR/zsh ~/.zsh
-ln -s ~/.zsh/prezto ~/.zprezto
-ln -s ~/.zsh/rc.zsh ~/.zshrc
-ln -s ~/.zsh/env.zsh ~/.zshenv
-ln -s ~/.zsh/profile.zsh ~/.zprofile
-ln -s ~/.zsh/login.zsh ~/.zlogin
-ln -s ~/.zsh/logout.zsh ~/.zlogout
-ln -s ~/.zsh/preztorc.zsh ~/.zpreztorc
+lns-once $HOUDINI_DIR/zsh ~/.zsh
+lns-once ~/.zsh/prezto ~/.zprezto
+lns-once ~/.zsh/rc.zsh ~/.zshrc
+lns-once ~/.zsh/env.zsh ~/.zshenv
+lns-once ~/.zsh/profile.zsh ~/.zprofile
+lns-once ~/.zsh/login.zsh ~/.zlogin
+lns-once ~/.zsh/logout.zsh ~/.zlogout
+lns-once ~/.zsh/preztorc.zsh ~/.zpreztorc
 
 # bash
 
-ln -s $HOUDINI_DIR/bash ~/.bash
-ln -s ~/.bash/profile.bash ~/.bash_profile
-ln -s ~/.bash/rc.bash ~/.bashrc
+lns-once $HOUDINI_DIR/bash ~/.bash
+lns-once ~/.bash/profile.bash ~/.bash_profile
+lns-once ~/.bash/rc.bash ~/.bashrc
 
-sudo echo $HOMEBREW_ZSH_BIN >> /etc/shells
+
+if ! $(cat /etc/shells | grep); then
+  log "appending ${HOMEBREW_ZSH_BIN} to /etc/shells"
+
+  echo $HOMEBREW_ZSH_BIN | sudo tee -a /etc/shells > /dev/null
+fi
 
 chsh -s $HOMEBREW_ZSH_BIN
 
 # tmux
 
-ln -s $HOUDINI_DIR/tmux.conf ~/.tmux.conf
+lns-once $HOUDINI_DIR/tmux.conf ~/.tmux.conf
 
 # rubocop
 
-ln -s $HOUDINI_DIR/rubocop.yml ~/.rubocop.yml
+lns-once $HOUDINI_DIR/rubocop.yml ~/.rubocop.yml
 
 # gem
 
-ln -s $HOUDINI_DIR/gemrc ~/.gemrc
+lns-once $HOUDINI_DIR/gemrc ~/.gemrc
 
 # git
 
-ln -s $HOUDINI_DIR/git/ignore ~/.gitignore
-ln -s $HOUDINI_DIR/git/config ~/.gitconfig
+lns-once $HOUDINI_DIR/git/ignore ~/.gitignore
+lns-once $HOUDINI_DIR/git/config ~/.gitconfig
 
 # ctags
 
-ln -s $HOUDINI_DIR/ctags ~/.ctags
+lns-once $HOUDINI_DIR/ctags ~/.ctags
 
 # leiningen
 
-ln -s $HOUDINI_DIR/lein ~/.lein
+lns-once $HOUDINI_DIR/lein ~/.lein
 
 # karabiner
 
-ln -s $HOUDINI_DIR/karabiner ~/.config
-
-# python
-
-local python_packages=(
-  'virtualenv'
-  'virtualenvwrapper'
-  'powerline-status'
-  'psutil'
-  'speedtest-cli'
-  'doge'
-  'neovim'
-)
-
-pip install --upgrade pip setuptools
-pip install ${python_packages}
-pip3 install --upgrade pip setuptools
-pip3 install ${python_packages}
-
-# ruby
-
-local ruby_gems=(
-  'boom'
-  'pry'
-  'rubocop'
-  'm'
-  'bundler'
-  'neovim'
-)
-
-ruby-install -s /tmp/ruby-src ruby
-
-gem install ${ruby_gems}
+lns-once $HOUDINI_DIR/karabiner ~/.config
 
 # heroku
 
 heroku plugins:install heroku-repo
 
 $HOUDINI_DIR/osx
-
