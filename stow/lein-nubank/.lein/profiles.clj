d67e89086cc39d47e5c2f01fbf5f42838158151c
Fuad Saud <fuadfsaud@gmail.com>
2019-09-07 23:12:39 -0300

    relein

--- stow/lein-nubank/.lein/profiles.clj
+++ stow/lein-nubank/.lein/profiles.clj
@@ -1,9 +1,20 @@
-{:user {:dependencies        [[cljdev "0.8.0"]]
-        :injections          [(require 'nu)]
-        :repositories        [["central"  {:url "https://repo1.maven.org/maven2/" :snapshots false}]
-                              ["clojars"  {:url "https://clojars.org/repo/"}]
-                              ["nu-maven" {:url    "s3p://nu-maven/releases/"
-                                           :region "sa-east-1"}]]
-        :plugin-repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
-        :plugins             [[s3-wagon-private "1.3.1" :upgrade false]
-                              [lein-ancient "0.6.15"]]}}
+{:user
+ {:dependencies        [[cljdev "0.8.0"]]
+  :injections          [(require 'nu)]
+  :repositories        [["central"  {:url "https://repo1.maven.org/maven2/"
+                                     :snapshots false}]
+                        ["clojars"  {:url "https://clojars.org/repo/"}]
+                        ["nu-maven" {:url "s3p://nu-maven/releases/"
+                                     :region "sa-east-1"}]]
+  :plugin-repositories [["nu-maven" {:url "s3p://nu-maven/releases/"}]]
+  :plugins             [[s3-wagon-private "1.3.1" :upgrade false]
+                        [lein-ancient "0.6.15"]]}
+
+ :repl {:plugins      [[cider/cider-nrepl "0.21.1"]
+                       [refactor-nrepl "2.4.0"]
+                       [lein-cljfmt "0.6.4"]]
+
+        :dependencies [[nrepl "0.6.0"]
+                       [mvxcvi/puget "1.1.1"]]
+
+        :repl-options {:timeout 120000}}}
--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,8 +1,10 @@
 {:user {:plugins      [[lein-ancient "0.6.15"]
                        [venantius/ultra "0.6.0"]]}
- :repl {:plugins      [[cider/cider-nrepl "0.21.1"]
+
+ :repl {:dependencies [[cljfmt "0.6.4"]
+                       [nrepl "0.6.0"]
+                       [mvxcvi/puget "1.1.2"]]
+
+        :plugins      [[cider/cider-nrepl "0.22.1"]
                        [refactor-nrepl "2.4.0"]
-                       [lein-cljfmt "0.6.4"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.13"]
-                       [cljfmt "0.6.4"]
-                       [mvxcvi/puget "1.1.2"]]}}
+                       [lein-cljfmt "0.6.4"]]}}
