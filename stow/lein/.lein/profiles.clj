209a35a2e5e77875d027fb1dae7e40ec6b36038b
Fuad Saud <fuadfsaud@gmail.com>
2019-10-01 11:04:08 -0300

    leins

--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,9 +1,11 @@
-{:user {:plugins      [[lein-ancient "0.6.15"]
-                       [venantius/ultra "0.6.0"]]}
+{:user {:dependencies   [[mvxcvi/puget "1.1.2"]]
+
+        :plugins        [[lein-ancient "0.6.15"]
+                         [lein-pprint "1.2.0"]
+                         [venantius/ultra "0.6.0"]]}
 
  :repl {:dependencies [[cljfmt "0.6.4"]
-                       [nrepl "0.6.0"]
-                       [mvxcvi/puget "1.1.2"]]
+                       [nrepl "0.6.0"]]
 
         :plugins      [[cider/cider-nrepl "0.22.1"]
                        [refactor-nrepl "2.4.0"]
