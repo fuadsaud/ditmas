65256ac1e8ec7a2a16668d23763dd73ffddabf75
Fuad Saud <fuadfsaud@gmail.com>
2019-10-26 18:47:25 -0300

    fix utils

--- setup/lib/utils.sh
+++ setup/lib/utils.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function echoerr {
-  (>&2 echo $1)
+  (>&2 echo "${1}")
 }
 
 function log {
@@ -9,14 +9,14 @@ function log {
 }
 
 function lns-once {
-  if [[ ! -h $2 ]]; then
-    ln -s $1 $2
+  if [[ ! -h ${2} ]]; then
+    ln -s "${1}" "${2}"
   fi
 }
 
 function case-linux-mac {
-  case `uname` in
-    Linux)  echo $1;;
-    Darwin) echo $2;;
+  case $(uname) in
+    Linux)  echo "${1}";;
+    Darwin) echo "${2}";;
   esac
 }
