bbe7b5c0cd779fda6fa09955281c5a7b24f61697
Fuad Saud <fuadfsaud@gmail.com>
2019-11-26 02:35:00 -0300

    propel

--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,4 +1,6 @@
-{:user {:dependencies   [[mvxcvi/puget "1.1.2"]]
+{:user {:dependencies   [[mvxcvi/puget "1.1.2"]
+                         [olical/propel "1.3.0"]]
+
 
         :plugins        [[lein-ancient "0.6.15"]
                          [lein-pprint "1.2.0"]
@@ -7,7 +9,7 @@
  :repl {:dependencies [[org.clojure/tools.namespace "0.3.1"]
                        [cljfmt "0.6.4"]
                        [nrepl "0.6.0"]
-                       [olical/propel "1.3.1"]
+
                        [mvxcvi/puget "1.1.2"]]
 
         :plugins      [[cider/cider-nrepl "0.22.1"]
