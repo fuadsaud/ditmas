c5296d1a66ed170cfdd48b3de9568905fe4d10ec
Fuad Saud <fuadfsaud@gmail.com>
2020-04-17 13:25:57 -0300

    lein-nubank

--- stow/lein-nubank/.lein/profiles.clj
+++ stow/lein-nubank/.lein/profiles.clj
@@ -15,21 +15,33 @@
                         [venantius/ultra "0.6.0"]
                         [lein-ancient "0.6.15"]
                         [lein-auto "0.1.3"]
-                        [com.jakemccrary/lein-test-refresh "0.24.1"]]}
+                        [com.jakemccrary/lein-test-refresh "0.24.1"]]
 
- :repl {:plugins      [[cider/cider-nrepl "0.21.1"]
-                       [refactor-nrepl "2.4.0"]
-                       [lein-cljfmt "0.6.4"]]
+  :repl-options {:prompt (constantly "user profile yeah=>")
+                 :init (do (println "Starting Propel...")
+                           (require 'propel.core)
+                           (let [prepl (propel.core/start-prepl! {:port-file? true})]
+                             (println "pREPL server started on port"
+                                      (:port prepl)
+                                      "on host"
+                                      (:address prepl))))}}
 
-        :dependencies [[org.clojure/tools.namespace "0.3.1"]
-                       [nrepl "0.6.0"]
-                       [olical/propel "1.3.0"]
-                       [mvxcvi/puget "1.1.1"]]}
+ :repl
+ {:plugins      [[cider/cider-nrepl "0.21.1"]
+                 [refactor-nrepl "2.4.0"]
+                 [lein-cljfmt "0.6.4"]]
 
- :repl-options {:init (do (println "Starting Propel...")
-                          (require 'propel.core)
-                          (let [prepl (propel.core/start-prepl! {:port-file? true})]
-                            (println "pREPL server started on port"
-                                     (:port prepl)
-                                     "on host"
-                                     (:address prepl))))}}
+  :dependencies [[org.clojure/tools.namespace "0.3.1"]
+                 [nrepl "0.6.0"]
+                 [olical/propel "1.3.0"]
+                 [mvxcvi/puget "1.1.1"]]}
+
+ :propel
+ {:repl-options {:prompt (constantly "CHIDDDDY=>")
+                 :init (do (println "Starting Propel...")
+                           (require 'propel.core)
+                           (let [prepl (propel.core/start-prepl! {:port-file? true})]
+                             (println "pREPL server started on port"
+                                      (:port prepl)
+                                      "on host"
+                                      (:address prepl))))}}}
