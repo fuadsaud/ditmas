bad16caaa94e22b7b36247e1331559e1f2e0e4bf
Fuad Saud <fuadfsaud@gmail.com>
2021-03-10 14:44:23 -0300

    zsh aliases

--- stow/zsh/.config/zsh/nu.zsh
+++ stow/zsh/.config/zsh/nu.zsh
@@ -5,14 +5,16 @@ function nbrp() { nu-br $@ --env prod    }
 function nmxs() { nu-mx $@ --env staging }
 function nmxp() { nu-mx $@ --env prod    }
 
-alias nufoo="nu aws credentials refresh"
+alias nuclone="nu projects clone"
+alias nucurl="nu services curl -f"
 alias nude="nu utils docker-env"
+alias nufoo="nu aws credentials refresh"
+alias numq="nu mordor query --accept application/json"
 alias nusss="nu security scope show"
 alias nussa="nu security scope add"
 alias nussf="nu security scope find"
-alias numq="nu mordor query --accept application/json"
-alias nuclone="nu projects clone"
-alias nucurl="nu services curl -f"
+alias nuvpnc="nu vpn connect --client openfortivpn"
+alias nuvpnd="nu vpn disconnect"
 alias pingbo="ping backoffice.nubank.com.br"
 alias zagallo="nu inutils zagallo"
 alias zoiper="~/.local/opt/Zoiper5/zoiper"
