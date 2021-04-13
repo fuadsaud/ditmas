58893f592ffabe59a91a9ed23071aba894f199a6
Fuad Saud <fuadfsaud@gmail.com>
2021-04-13 11:52:56 -0300

    Update randr configs

--- stow/randr/.config/screenlayout/home-2-way.sh
+++ stow/randr/.config/screenlayout/home-2-way.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate left --output DP-2 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output HDMI-2 --off
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output HDMI-2 --off
