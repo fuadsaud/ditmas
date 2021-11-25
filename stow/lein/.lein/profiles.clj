e36501dc7d9922346997f0f407aad0b753a99d71
Fuad Saud <fuadfsaud@gmail.com>
2021-11-25 19:00:13 -0300

    lein cleanup

--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,17 +1,8 @@
-{:user {:dependencies   [[mvxcvi/puget "1.1.2"]
-                         [olical/propel "1.3.0"]]
+{:user {:dependencies [[org.clojure/tools.namespace "1.1.0"]]
 
-        :plugins        [[lein-ancient "0.6.15"]
-                         [lein-pprint "1.2.0"]
-                         [venantius/ultra "0.6.0"]
-                         [com.jakemccrary/lein-test-refresh "0.24.1"]]}
-
- :repl {:dependencies [[org.clojure/tools.namespace "0.3.1"]
-                       [cljfmt "0.6.4"]
-                       [nrepl "0.6.0"]
-
-                       [mvxcvi/puget "1.1.2"]]
-
-        :plugins      [[cider/cider-nrepl "0.22.1"]
-                       [refactor-nrepl "2.4.0"]
-                       [lein-cljfmt "0.6.4"]]}}
+        :plugins [[lein-ancient "1.0.0-RC3"]
+                  [com.jakemccrary/lein-test-refresh "0.25.t"]
+                  [nrepl "0.9.0-beta5"]
+                  [cljfmt "0.8.0"]
+                  [cider/cider-nrepl "0.27.2"]
+                  [refactor-nrepl "3.1.0"]]}}
