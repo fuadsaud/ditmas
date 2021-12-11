c94d4d6b408bccb6c48bcc7995d2758abcdc526e
Fuad Saud <fuadfsaud@gmail.com>
2021-12-11 19:13:20 -0300

    fix lein

--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:dependencies [[org.clojure/tools.namespace "1.1.0"]]
 
         :plugins [[lein-ancient "1.0.0-RC3"]
-                  [com.jakemccrary/lein-test-refresh "0.25.t"]
+                  [com.jakemccrary/lein-test-refresh "0.25.0"]
                   [nrepl "0.9.0-beta5"]
                   [cljfmt "0.8.0"]
                   [cider/cider-nrepl "0.27.2"]
