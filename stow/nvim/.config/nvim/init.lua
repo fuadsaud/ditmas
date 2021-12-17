eeb3787f1052d1be103cdd79668d277e7cfa60a5
Fuad Saud <fuadfsaud@gmail.com>
2021-12-17 00:10:15 -0300

    fix airline

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -7,3 +7,7 @@ config_files = { 'plug-helpers.vim', 'plug.vim' }
 for _, config_file in ipairs(config_files) do
   source(config_file)
 end
+
+-- For some reason setting this in the after/plugin/airline.vim has no effect;
+-- setting it here solves it.
+vim.g.airline_powerline_fonts = 1
