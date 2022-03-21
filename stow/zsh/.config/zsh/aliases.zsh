3a2b992e0f8741d4631bdcaf6e9ee78673faad25
Fuad Saud <fuadfsaud@gmail.com>
2022-03-21 17:08:00 +0100

    cleanup aliases

--- stow/zsh/.config/zsh/aliases.zsh
+++ stow/zsh/.config/zsh/aliases.zsh
@@ -1,7 +1,9 @@
+alias a="rg"
 alias bat='docker run -it --rm -e BAT_THEME -e BAT_STYLE -e BAT_TABS -v "$(pwd):/myapp" danlynn/bat'
 alias bx="bundle exec"
 alias cdgr="cd \`git root\`"
 alias dsnc="dsync"
+alias g="git"
 alias mk="make"
 alias n="nvim"
 alias nexus="emulator -avd Nexus_5X_API_28"
@@ -9,12 +11,10 @@ alias nvplug="nvim +PlugInstall +qall"
 alias p="paru"
 alias pacs="sudo pacman -S"
 alias qmkxf="qmk flash -kb xd75 -km fuad -bl dfu"
+alias qmkef="qmk flash -kb ergodash/rev1 -km fuad"
 alias rk="rake"
 alias sys="sudo systemctl"
 alias sysu="systemctl --user"
-alias vhalt="vagrant halt"
-alias vssh="vagrant ssh"
-alias vup="vagrant up"
 alias vi="vim"
 alias vplug="vim +PlugInstall +qall"
 alias w1="watch -n 10"
@@ -30,14 +30,15 @@ alias ln="${aliases[ln]:-ln} -v"
 alias mv="${aliases[mv]:-mv} -v"
 alias rm="${aliases[rm]:-rm} -v"
 
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i' # cd with interactive selection
+
+# alias a='fasd -a'        # any
+# alias s='fasd -si'       # show / search / select
+# alias d='fasd -d'        # directory
+# alias f='fasd -f'        # file
+# alias sd='fasd -sid'     # interactive directory selection
+# alias sf='fasd -sif'     # interactive file selection
+# alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+# alias zz='fasd_cd -d -i' # cd with interactive selection
 alias j="fasd_jump"
 alias nf="fasd_edit"
 
