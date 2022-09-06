784439d41417185616031ebcec8347fee4970aaa
Fuad Saud <fuadfsaud@gmail.com>
2022-09-06 14:05:33 +0200

    bump lein

--- stow/lein/.lein/profiles.clj
+++ stow/lein/.lein/profiles.clj
@@ -1,9 +1,15 @@
-{:user {:dependencies [[org.clojure/tools.namespace "1.1.0"]]
+{:user {:dependencies [[org.clojure/tools.namespace "1.3.0"]
+                       [hashp "0.2.1"]
+                       [com.github.liquidz/antq "RELEASE"]]
 
         :plugins [[lein-ancient "1.0.0-RC3"]
                   [lein-cprint "1.3.3"]
                   [com.jakemccrary/lein-test-refresh "0.25.0"]
-                  [nrepl "0.9.0-beta5"]
-                  [cljfmt "0.8.0"]
-                  [cider/cider-nrepl "0.27.2"]
-                  [refactor-nrepl "3.1.0"]]}}
+                  [nrepl "0.9.0"]
+                  [cljfmt "0.8.2"]
+                  [cider/cider-nrepl "0.28.5"]
+                  [refactor-nrepl "3.5.4"]]
+
+        ; :aliases {"outdated" ["run" "-m" "antq.core"]}
+
+        :injections [(require 'hashp.core)]}}
