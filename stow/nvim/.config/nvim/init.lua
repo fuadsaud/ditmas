825b2cd0ddc9018c4576310e2f14ba6df65edbda
Fuad Saud <fuadfsaud@gmail.com>
2023-01-17 17:13:14 +0100

    wipe airline

--- stow/nvim/.config/nvim/init.lua
+++ stow/nvim/.config/nvim/init.lua
@@ -4,7 +4,3 @@ vim.g["aniseed#env"] = {
   module = "config.init",
   compile = true
 }
-
--- For some reason setting this in the after/plugin/airline.vim has no effect;
--- setting it here solves it.
-vim.g.airline_powerline_fonts = 1
